01d677aaf16ead270f7c583adb6113085a7dacc0
AhmadjanovD <halilullahahmadjanov@gmail.com>
2022-08-10 06:13:57 +0900

    handleBars

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
-const express = require('express')
+const express = require('express');
+const {engine} = require('express-handlebars');
 
 const app = express()
-
-app.use('/api/books', require('./routes/routes'))
 
 //Body parser
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
+
+app.engine('.hbs', engine({ extname: '.hbs' }))
+app.set('view engine', '.hbs')
+
+app.use('/', require('./routes/home'))
+app.use('/api/books', require('./routes/routes'))
 
 const PORT = process.env.PORT || 3000
 
--- routes/routes.js
+++ routes/routes.js
@@ -9,7 +9,7 @@ router.get('/',(req, res)=>{
 })
 
 // Get a book by id
-router.get('/', (req, res)=>{
+router.get('/:id', (req, res)=>{
    const isExist = books.some(book => book.id == req.params.id)
    if(isExist) {
     res.json(books.filter(book => book.id == req.params.id))
@@ -25,12 +25,45 @@ router.post('/', (req, res)=>{
         pages: req.body.pages,
         author: req.body.author
     }
-    
-    if(!req.body.name || !req.body.author || !req.body.pages) {
-       return res.status(400).json({message: 'Please enter all informations'})
+
+    if(!req.body.name || !req.body.author || !req.body.pages){
+        return res.status(400).json({message:'Please enter all required informations'})
     }
+
     books.push(newBook)
     res.json(books)
 })
+
+// Edit book by id
+router.put('/:id', (req, res)=>{
+    const isExist = books.some(book => book.id == req.params.id)
+    if(isExist) {
+        const updateBook = req.body
+
+        books.forEach(book => {
+            if(book.id === parseInt(req.params.id)){
+                book.name = updateBook.name ? updateBook.name : book.name
+                book.author = updateBook.author ? updateBook.author : book.author
+                book.pages = updateBook.pages ? updateBook.pages : book.pages
+
+                res.json({message: 'Book information updated successfully', books})
+            }
+        })
+    }else {
+     res.status(404).json({message:`The book ${req.params.id} is not available`})
+    }
+ })
+
+ router.delete('/:id', (req, res) => {
+    const isExist = books.some(book => book.id === parseInt(req.params.id))
+   if(isExist) {
+    res.json({
+        message: 'Book is deleted successfully',
+        books: books.filter(book => book.id !== parseInt(req.params.id))
+    })
+   }else {
+    res.status(404).json({message:`The book ${req.params.id} is not available`})
+   }
+ })
 
 module.exports = router
